6167e373a3856753d76a90b9f8186743cd3dff4d
alrikmunoz <50486916+alrikmunoz@users.noreply.github.com>
2022-04-17 13:52:15 +0200

    large overhaul, API more functional

--- js/seats.mjs
+++ js/seats.mjs
@@ -6,13 +6,9 @@
  * 
  * Example usage:
  * 
- * let seats = new Seats();  // by default loads with the 8 default parties
- * 
- * seats.setPercentage("S", 40)
- * seats.setPercentage("M", 30)
- * seats.setPercentage("SD", 30)
- * 
- * console.log(seats.getAllocation())
+ * let seats = new Seats()
+ * let allocation = seats.allocate(Seats.election2018)
+ * console.log(allocation)
  * 
  * - - - - - - - - - - - - - - - - - - - - - - - 
  * 
@@ -23,16 +19,16 @@ class Constants {
     static threshold = 4;
     static startDivisor = 1.2;
     static seats = 349;
-    static defaultParties = ["S", "M", "SD", "C", "V", "KD", "L", "MP"]
 }
 
 class Party {
-    constructor(name) {
+    constructor(name, percentage) {
         this.name = name;
         this.seats = 0;
         this.score = 0;
-        this.percentage = 0;
+        this.percentage = percentage;
         this.divisor = Constants.startDivisor;
+        this.updateScore();
     }
 
     /**
@@ -47,114 +43,65 @@ class Party {
 
 
 export default class Seats {
-    parties = [];
+    static election2018 = { "S":28.26, "M":19.84, "SD":17.53, "C":8.61, "V":8.00, "KD":6.32, "L":5.49, "MP":4.41 }
 
     /**
-     * Creates a Seats object which is used for all interaction, e.g. let seats = new Seats(); seats.getAllocation();
-     * @param {boolean} useDefault 
+     * Given an object of partyname:voteshare key-value pairs, returns an object with partyname:seats key-value pairs
+     * in accordance with the Modified Sainte-Laguë method (sv: jämkade uddatalsmetoden)
+     * @param {Object.<string, number} votes object of votes to parties, with (partyname : voteshare) key-value pairs
+     * @returns {Object.<string, number} object of seats allocated to parties, with (partyname : seats) key value pairs
      */
-    constructor(useDefault = true) {
-        if (useDefault)
-            Constants.defaultParties.forEach(partyname => {
-                this.addParty(partyname);
-            });
-    }
+    allocate(votes) {
 
-    /**
-     * Adds party with partyname as name to current calculation. Initial percentage is 0 %
-     * @param {string} partyname
-     */
-    addParty(partyname) {
-        let party = this._getParty(partyname);
-        if (party !== undefined) {
-            console.error("Error: Cannot add same party twice");
-            return;
-        }
+        let parties = Object.keys(votes).map(partyname => new Party(partyname, votes[partyname])); // array of parties
         
-        this.parties.push(new Party(partyname))
+        // disregard parties below threshold when allocating seats
+        let validParties = parties.filter(party => party.percentage >= Constants.threshold);
+        
+        if (validParties.length !== 0) {
+            let remainingSeats = Constants.seats;
+            while (remainingSeats > 0) {
+                // TODO: Consider ties (e.g. draw random)
 
-        this._allocateSeats();
-    }
+                // find party with highest score
+                let highestScoringParty = validParties.reduce((prev, curr) => curr.score > prev.score ? curr : prev);
 
-    /**
-     * Sets a new percentage for the given party and optionally reallocates seats to current parties
-     * @param {string} partyname the name of the party
-     * @param {number} percentage the new percentage of the party
-     * @param {boolean} reallocate whether seats shall be reallocated after percentage is set, default is true
-     */
-    setPercentage(partyname, percentage, reallocate = true) {
-        let party = this._getParty(partyname);
-        if (party === undefined) {
-            console.error("Error: Unknown party", partyname);
-            return;
+                // give 1 seat to party
+                highestScoringParty.seats++;
+                highestScoringParty.updateScore();
+
+                remainingSeats--;
+            }
         }
 
-        party.percentage = percentage;
-        
-        if (reallocate)
-            this._allocateSeats();
-    }
-
-    /**
-     * Returns the current allocation of seats to the involved parties as an object on the form of
-     * { partyname : seats, [...] }
-     * 
-     * @returns {Object.<string, number>} a list of objects with the keys name and seats
-     */
-    getAllocation() {
-        return this.parties.reduce((allocation, party) => (allocation[party.name] = party.seats, allocation), {});
+        return this._getAllocation(parties)
     }
 
     /**
      * Returns true if percentages of parties add up to 100.0 %  (floating point 32-bit precision)
+     * @param {Object.<string, number>} votes an object made up of key-value pairs of partyname : voteshare
      * @returns {boolean} whether parties add up to 100 %
      */
-    checkPartySizes() {
-        let totalPercentage = Math.fround(this.parties.reduce((sum, party) => sum + party.percentage, 0));
-        return totalPercentage == 100;
+    checkPartySizes(votes) {
+        return this.summarizeValues(votes) == 100;
     }
 
     /**
-     * Resets allocation data for all parties
+     * Returns the sum of an object's values, where the object is made up of key-value pairs of key : number.
+     * @param {Object.<*,number>} object object whose values shall be summarized
+     * @returns {number} the sum
      */
-    _resetAllocation() {
-        this.parties.forEach(party => {
-            party.divisor = Constants.startDivisor;
-            party.seats = 0;
-            party.updateScore();
-        })
+    summarizeValues(object) {
+        return Math.fround(Object.keys(object).reduce((sum, key) => sum + object[key], 0));
     }
-
     /**
-     * Gives out seats to the added parties in accordance with the Modified Sainte-Laguë method (sv: jämkade uddatalsmetoden)
+     * Given an array of {@link Party} objects, returns the current allocation of seats among them as an object on the form of
+     * { partyname : seats, [...] }
+     * 
+     * @param {Party[]} parties the array of parties
+     * @returns {Object.<string, number>} a list of objects with the keys name and seats
      */
-    _allocateSeats() {
-        this._resetAllocation();
-
-        // disregard parties below threshold when allocating seats
-        let validParties = this.parties.filter(party => party.percentage >= Constants.threshold);
-        if (validParties.length == 0) return;
-
-        let remainingSeats = Constants.seats;
-        while (remainingSeats > 0) {
-            // TODO: Consider ties (e.g. draw random)
-            // find party with highest score
-            let highestScoringParty = validParties.reduce((prev, curr) => curr.score > prev.score ? curr : prev);
-
-            // give 1 seat to party
-            highestScoringParty.seats++;
-            highestScoringParty.updateScore();
-
-            remainingSeats--;
-        }
-    }
-
-    /**
-     * Given a party name returns the party with that name, or undefined if no such party is found in current parties
-     * @param {string} partyname 
-     * @returns {Party|undefined} party with given partyname or undefined if not found in current parties
-     */
-    _getParty(partyname) {
-        return this.parties.find(party => party.name == partyname)
+    _getAllocation(parties) {
+        return parties.reduce((allocation, party) => (allocation[party.name] = party.seats, allocation), {});
     }
 }
